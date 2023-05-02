01e192c64a1bdff656323b04889c8f07206fb44b
jackson <nervisnervous@gmail.com>
2023-05-02 18:32:20 -0400

    Bug fixes: `1.0.4`
    Forgot to export all functions lol

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,29 @@ import { default as GetUsername } from './functions/Users/GetUsername';
 import { default as GetUserDisplayName } from './functions/Users/GetUserDisplayName';
 import { default as UserSearch } from './functions/Users/UserSearch';
 import { default as GetUsernameHistory } from './functions/Users/GetUsernameHistory';
+import { default as GetUserId } from './functions/Users/GetUserId';
 
-export { GetUserAvatar, GetUserAvatarHeadshot, GetUsername, GetUserDisplayName, UserSearch, GetUsernameHistory };
+import { default as ShortenAssetUrl } from './functions/Shorten/ShortenAssetUrl';
+import { default as ShortenClothingUrl } from './functions/Shorten/ShortenClothingUrl';
+import { default as ShortenDevforumUrl } from './functions/Shorten/ShortenDevforumUrl';
+import { default as ShortenDevforumUserUrl } from './functions/Shorten/ShortenDevforumUserUrl';
+import { default as ShortenGameUrl } from './functions/Shorten/ShortenGameUrl';
+import { default as ShortenGroupUrl } from './functions/Shorten/ShortenGroupUrl';
+import { default as ShortenUserUrl } from './functions/Shorten/ShortenUserUrl';
+
+export {
+  GetUserAvatar,
+  GetUserAvatarHeadshot,
+  GetUsername,
+  GetUserDisplayName,
+  UserSearch,
+  GetUsernameHistory,
+  GetUserId,
+  ShortenAssetUrl,
+  ShortenClothingUrl,
+  ShortenDevforumUrl,
+  ShortenDevforumUserUrl,
+  ShortenGameUrl,
+  ShortenGroupUrl,
+  ShortenUserUrl
+};
