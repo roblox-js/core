0949a6a83c1eab4c388d0a863402a3adf5820001
yiaeula <77604356+lqvers@users.noreply.github.com>
2025-03-14 20:11:28 -0400

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,16 @@
-import { default as GetUserAvatar } from './functions/Users/GetUserAvatar';
-import { default as GetUserAvatarHeadshot } from './functions/Users/GetUserAvatarHeadshot';
-import { default as GetUsername } from './functions/Users/GetUsername';
-import { default as GetUserDisplayName } from './functions/Users/GetUserDisplayName';
-import { LimitOptions, default as UserSearch } from './functions/Users/UserSearch';
-import { default as GetUsernameHistory } from './functions/Users/GetUsernameHistory';
-import { default as GetUserId } from './functions/Users/GetUserId';
-
-import { default as ShortenAssetUrl } from './functions/Shorten/ShortenAssetUrl';
-import { default as ShortenClothingUrl } from './functions/Shorten/ShortenClothingUrl';
-import { default as ShortenDevforumUrl } from './functions/Shorten/ShortenDevforumUrl';
-import { default as ShortenDevforumUserUrl } from './functions/Shorten/ShortenDevforumUserUrl';
-import { default as ShortenGameUrl } from './functions/Shorten/ShortenGameUrl';
-import { default as ShortenGroupUrl } from './functions/Shorten/ShortenGroupUrl';
-import { default as ShortenUserUrl } from './functions/Shorten/ShortenUserUrl';
-import { default as CheckCookie } from './functions/Account/CheckAuth';
-import { default as SendFriendRequest } from './functions/Account/SendFriendRequest';
-
-export {
-  GetUserAvatar,
-  GetUserAvatarHeadshot,
-  GetUsername,
-  GetUserDisplayName,
-  UserSearch,
-  GetUsernameHistory,
-  GetUserId,
-  ShortenAssetUrl,
-  ShortenClothingUrl,
-  ShortenDevforumUrl,
-  ShortenDevforumUserUrl,
-  ShortenGameUrl,
-  ShortenGroupUrl,
-  ShortenUserUrl,
-  CheckCookie,
-  //SendFriendRequest
-};
+export { default as GetUserAvatar } from './functions/Users/GetUserAvatar';
+export { default as GetUserAvatarHeadshot } from './functions/Users/GetUserAvatarHeadshot';
+export { default as GetUsername } from './functions/Users/GetUsername';
+export { default as GetUserDisplayName } from './functions/Users/GetUserDisplayName';
+export { LimitOptions, default as UserSearch } from './functions/Users/UserSearch';
+export { default as GetUsernameHistory } from './functions/Users/GetUsernameHistory';
+export { default as GetUserId } from './functions/Users/GetUserId';
+export { default as ShortenAssetUrl } from './functions/Shorten/ShortenAssetUrl';
+export { default as ShortenClothingUrl } from './functions/Shorten/ShortenClothingUrl';
+export { default as ShortenDevforumUrl } from './functions/Shorten/ShortenDevforumUrl';
+export { default as ShortenDevforumUserUrl } from './functions/Shorten/ShortenDevforumUserUrl';
+export { default as ShortenGameUrl } from './functions/Shorten/ShortenGameUrl';
+export { default as ShortenGroupUrl } from './functions/Shorten/ShortenGroupUrl';
+export { default as ShortenUserUrl } from './functions/Shorten/ShortenUserUrl';
+export { default as CheckCookie } from './functions/Account/CheckAuth';
+// WIP: export { default as SendFriendRequest } from './functions/Account/SendFriendRequest';
